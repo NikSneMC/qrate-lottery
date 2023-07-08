9841548e81ed7ee74fcc42a95499604a6097e2a7
NikSne <niksne.mc@yandex.ru>
2023-07-08 10:47:19 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { Window } from "@/components/window"
 import { styled } from "styled-components"
 import { Input } from "@/components/input"
 import { useState } from "react"
-import { WindowLink } from "@/components/window/window-link"
 
 const Spacer = styled.div`
   flex: 1;
@@ -29,37 +28,50 @@ const ButtonWrapper = styled.div`
 `
 
 export default function Home() {
-  const [firstValue, setFirstValue] = useState<string>("")
-  const [lastValue, setLastValue] = useState<string>("")
-  const [amount, setAmount] = useState<string>("")
+    const [firstValue, setFirstValue] = useState<string>("");
+    const [lastValue, setLastValue] = useState<string>("");
+    const [amount, setAmount] = useState<string>("");
 
-  return (
-    <Window headerText="Квантовая лотерея">
-      <Spacer />
-      <Inputs>
-        <Input
-          inputTitle="Первый билет"
-          inputValue={firstValue}
-          inputUpdateValue={setFirstValue}
-        />
-        <Input
-          inputTitle="Последний билет"
-          inputValue={lastValue}
-          inputUpdateValue={setLastValue}
-        />
-        <Input
-          inputTitle="Количество призов"
-          inputValue={amount}
-          inputUpdateValue={setAmount}
-        />
-      </Inputs>
-      <ButtonWrapper>
-        <WindowLink
-          href={`/results?minValue=${firstValue}&maxValue=${lastValue}&amount=${amount}`}
-        >
-          Запуск
-        </WindowLink>
-      </ButtonWrapper>
-    </Window>
-  )
+    const handleClick = () => {
+        const minValue = parseInt(firstValue);
+        const maxValue = parseInt(lastValue);
+        const prizeAmount = parseInt(amount);
+
+        if (!Number.isInteger(minValue) || !Number.isInteger(maxValue) || !Number.isInteger(prizeAmount || minValue <= 0 || maxValue <= 0 || prizeAmount <= 0)) {
+            alert("Некорректный ввод: все числа должны быть натуральными.");
+            return;
+        }
+
+        if (minValue >= maxValue) {
+            alert("Некорректный ввод: номер первого билета должен быть меньше, чем номер второго билета")
+        }
+
+        window.location.href = `/results?minValue=${minValue}&maxValue=${maxValue}&amount=${prizeAmount}`;
+    };
+
+    return (
+        <Window headerText="Квантовая лотерея">
+            <Spacer />
+            <Inputs>
+                <Input
+                    inputTitle="Первый билет"
+                    inputValue={firstValue}
+                    inputUpdateValue={setFirstValue}
+                />
+                <Input
+                    inputTitle="Последний билет"
+                    inputValue={lastValue}
+                    inputUpdateValue={setLastValue}
+                />
+                <Input
+                    inputTitle="Количество призов"
+                    inputValue={amount}
+                    inputUpdateValue={setAmount}
+                />
+            </Inputs>
+            <ButtonWrapper>
+                <button onClick={handleClick}>Запуск</button>
+            </ButtonWrapper>
+        </Window>
+    );
 }
